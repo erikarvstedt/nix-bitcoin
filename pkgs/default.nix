0fd7a8bdbb44ffbd2a813e2ced44979a506cf070
Erik Arvstedt <erik.arvstedt@gmail.com>
2019-11-27 14:04:31 +0100

    add generate-secrets pkg
    generate-secrets.sh will also be used in generate-secrets.nix, so DRY
    its dependency definitions.

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -14,4 +14,5 @@
   hwi = pkgs.callPackage ./hwi { };
   pylightning = pkgs.python3Packages.callPackage ./pylightning { };
   liquid-swap = pkgs.python3Packages.callPackage ./liquid-swap { };
+  generate-secrets = pkgs.callPackage ./generate-secrets { };
 }
