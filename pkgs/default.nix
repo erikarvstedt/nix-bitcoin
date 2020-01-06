f04122c9695d65fa2f3d9a21d39a4fcdfb6e3394
Erik Arvstedt <erik.arvstedt@gmail.com>
2020-01-06 21:54:49 +0100

    fixup! remove stray entries from pkgs/default.nix

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,11 +1,6 @@
 { pkgs ? import <nixpkgs> {} }:
 {
-  # 'lib', 'modules' and 'overlays' are special, see
-  # https://github.com/nix-community/NUR for more.
-  modules = import ./modules; # NixOS modules
-
   nodeinfo = pkgs.callPackage ./nodeinfo { };
-  banlist = pkgs.callPackage ./banlist { };
   lightning-charge = pkgs.callPackage ./lightning-charge { };
   nanopos = pkgs.callPackage ./nanopos { };
   spark-wallet = pkgs.callPackage ./spark-wallet { };
