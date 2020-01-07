8529d9143cab3389ce6145df84a751c996798653
Erik Arvstedt <erik.arvstedt@gmail.com>
2020-01-07 01:33:16 +0100

    fixup! add rationale for services.allowSecretsDirAccess

--- network/network.nix
+++ network/network.nix
@@ -10,6 +10,11 @@
         secretsFile = ../secrets/secrets.nix;
       }).activeSecrets;
 
+      # nixops makes the secrets directory accessible only for users with group 'key'.
+      # For compatibility with other deployment methods besides nixops, we forego the
+      # use of the 'key' group and make the secrets dir world-readable instead.
+      # This is safe because all containing files have their specific private
+      # permissions set.
       systemd.services.allowSecretsDirAccess = {
         requires = [ "keys.target" ];
         after = [ "keys.target" ];
