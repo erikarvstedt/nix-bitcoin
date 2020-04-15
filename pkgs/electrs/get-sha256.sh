a60c3764a1c3455ece2c0b18517cb928832eca9e
Erik Arvstedt <erik.arvstedt@gmail.com>
2020-04-15 18:43:58 +0200

    strip whitespace, simplify comments

--- pkgs/electrs/get-sha256.sh
+++ pkgs/electrs/get-sha256.sh
@@ -2,25 +2,22 @@
 #! nix-shell -i bash -p git gnupg
 set -euo pipefail
 
-# Creating temporary directory
-echo "Creating temporary directory"
 TMPDIR="$(mktemp -d)"
 trap "rm -rf $TMPDIR" EXIT
 cd $TMPDIR
-git clone https://github.com/romanz/electrs 2> /dev/null
 
-# Checking out latest release
-echo "Checking out latest release"
+echo "Fetching latest release"
+git clone https://github.com/romanz/electrs 2> /dev/null
 cd electrs
 latesttagelectrs=$(git describe --tags `git rev-list --tags --max-count=1`)
 echo "Latest release is ${latesttagelectrs}"
 
-# GPG Verification
+# GPG verification
 export GNUPGHOME=$TMPDIR
-gpg2 --fetch-key https://keybase.io/romanz/pgp_keys.asc?fingerprint=15c8c3574ae4f1e25f3f35c587cae5fa46917cbb 
+gpg2 --fetch-key https://keybase.io/romanz/pgp_keys.asc?fingerprint=15c8c3574ae4f1e25f3f35c587cae5fa46917cbb
 echo "Verifying latest release"
 git verify-tag ${latesttagelectrs}
 
-# Calculating sha256
+# Calculate sha256
 # The prefix option is necessary because GitHub prefixes the archive contents in this format
 echo "sha256 for ${latesttagelectrs} is $(git archive --format tar.gz --prefix=electrs-"${latesttagelectrs//v}"/ ${latesttagelectrs} | sha256sum )"
