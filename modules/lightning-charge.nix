2475957287a127e9dfbc54ce10929a576fa6c530
Erik Arvstedt <erik.arvstedt@gmail.com>
2020-05-18 22:42:17 +0200

    fixup! lightning-charge: add dedicated user
    extract user/group variables for better readability

--- modules/lightning-charge.nix
+++ modules/lightning-charge.nix
@@ -5,6 +5,8 @@ with lib;
 let
   cfg = config.services.lightning-charge;
   inherit (config) nix-bitcoin-services;
+  user = config.users.users.lightning-charge.name;
+  group = config.users.users.lightning-charge.group;
 in {
   options.services.lightning-charge = {
     enable = mkOption {
@@ -30,7 +32,7 @@ in {
     users.groups.lightning-charge = {};
 
     systemd.tmpfiles.rules = [
-      "d '${cfg.dataDir}' 0700 ${config.users.users.lightning-charge.name} ${config.users.users.lightning-charge.group} - -"
+      "d '${cfg.dataDir}' 0700 ${user} ${group} - -"
     ];
 
     environment.systemPackages = [ pkgs.nix-bitcoin.lightning-charge ];
@@ -44,7 +46,7 @@ in {
         # TODO: Remove eventually
         if [[ -e ${config.services.clightning.dataDir}/lightning-charge.db ]]; then
           mv ${config.services.clightning.dataDir}/lightning-charge.db ${cfg.dataDir}/lightning-charge.db
-          chown ${config.users.users.lightning-charge.name}:${config.users.users.lightning-charge.group} ${cfg.dataDir}/lightning-charge.db
+          chown ${user}: ${cfg.dataDir}/lightning-charge.db
           chmod 600 ${cfg.dataDir}/lightning-charge.db
         fi
         '';
@@ -52,13 +54,13 @@ in {
           PermissionsStartOnly = "true";
           EnvironmentFile = "${config.nix-bitcoin.secretsDir}/lightning-charge-env";
           ExecStart = "${pkgs.nix-bitcoin.lightning-charge}/bin/charged -l ${config.services.clightning.dataDir}/bitcoin -d ${cfg.dataDir}/lightning-charge.db";
-          User = "${config.users.users.lightning-charge.name}";
+          User = user;
           Restart = "on-failure";
           RestartSec = "10s";
       } // nix-bitcoin-services.defaultHardening
         // nix-bitcoin-services.nodejs
         // nix-bitcoin-services.allowTor;
     };
-    nix-bitcoin.secrets.lightning-charge-env.user = "${config.users.users.lightning-charge.name}";
+    nix-bitcoin.secrets.lightning-charge-env.user = user;
   };
 }
