506c7c2cb8add3c4fc987c2616063bf4fa3713b8
nixbitcoin <nixbitcoin@i2pmail.org>
2020-05-18 14:32:17 +0000

    lightning-charge: add dedicated user
    fixup! lightning-charge: fix formatting

--- modules/lightning-charge.nix
+++ modules/lightning-charge.nix
@@ -14,35 +14,51 @@ in {
         If enabled, the lightning-charge service will be installed.
       '';
     };
-    clightning-datadir = mkOption {
-      type = types.str;
-      default = "/var/lib/clighting/";
-      description = ''
-        Data directory of the clightning service
-      '';
+    dataDir = mkOption {
+      type = types.path;
+      default = "/var/lib/lightning-charge";
+      description = "The data directory for lightning-charge.";
     };
   };
 
   config = mkIf cfg.enable {
+    users.users.lightning-charge = {
+      description = "lightning-charge User";
+      group = "lightning-charge";
+      extraGroups = [ "clightning" ];
+    };
+    users.groups.lightning-charge = {};
+
+    systemd.tmpfiles.rules = [
+      "d '${cfg.dataDir}' 0700 ${config.users.users.lightning-charge.name} ${config.users.users.lightning-charge.group} - -"
+    ];
+
     environment.systemPackages = [ pkgs.nix-bitcoin.lightning-charge ];
     systemd.services.lightning-charge = {
       description = "Run lightning-charge";
       wantedBy = [ "multi-user.target" ];
       requires = [ "clightning.service" ];
       after = [ "clightning.service" ];
+      preStart = ''
+        # Move existing lightning-charge.db
+        # TODO: Remove eventually
+        if [[ -e ${config.services.clightning.dataDir}/lightning-charge.db ]]; then
+          mv ${config.services.clightning.dataDir}/lightning-charge.db ${cfg.dataDir}/lightning-charge.db
+          chown ${config.users.users.lightning-charge.name}:${config.users.users.lightning-charge.group} ${cfg.dataDir}/lightning-charge.db
+          chmod 600 ${cfg.dataDir}/lightning-charge.db
+        fi
+        '';
       serviceConfig = {
+          PermissionsStartOnly = "true";
           EnvironmentFile = "${config.nix-bitcoin.secretsDir}/lightning-charge-env";
-          ExecStart = "${pkgs.nix-bitcoin.lightning-charge}/bin/charged -l ${config.services.clightning.dataDir}/bitcoin -d ${config.services.clightning.dataDir}/lightning-charge.db";
-          # Unfortunately c-lightning doesn't allow setting the permissions of the rpc socket,
-          # so this must run as the clightning user
-          # https://github.com/ElementsProject/lightning/issues/1366
-          User = "clightning";
+          ExecStart = "${pkgs.nix-bitcoin.lightning-charge}/bin/charged -l ${config.services.clightning.dataDir}/bitcoin -d ${cfg.dataDir}/lightning-charge.db";
+          User = "${config.users.users.lightning-charge.name}";
           Restart = "on-failure";
           RestartSec = "10s";
       } // nix-bitcoin-services.defaultHardening
         // nix-bitcoin-services.nodejs
         // nix-bitcoin-services.allowTor;
     };
-    nix-bitcoin.secrets.lightning-charge-env.user = "clightning";
+    nix-bitcoin.secrets.lightning-charge-env.user = "${config.users.users.lightning-charge.name}";
   };
 }
