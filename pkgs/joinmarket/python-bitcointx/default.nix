bf67206fa6381cf457ed8f2de0cc028e5380662a
Erik Arvstedt <erik.arvstedt@gmail.com>
2020-09-04 10:37:54 +0200

    python-bitcointx: fix provision of openssl dependency
    previously, openssl was only available to the package build environment, but not in
    the final build.

--- pkgs/joinmarket/python-bitcointx/default.nix
+++ pkgs/joinmarket/python-bitcointx/default.nix
@@ -11,13 +11,13 @@ buildPythonPackage rec {
     sha256 = "35edd694473517508367338888633954eaa91b2622b3caada8fd3030ddcacba2";
   };
 
-  propagatedBuildInputs = [ secp256k1 openssl ];
-
   patchPhase = ''
     for path in core/secp256k1.py tests/test_load_secp256k1.py; do
       substituteInPlace "bitcointx/$path" \
         --replace "ctypes.util.find_library('secp256k1')" "'${secp256k1}/lib/libsecp256k1.so'"
     done
+    substituteInPlace bitcointx/core/key.py \
+      --replace "ctypes.util.find_library('ssl')" "'${openssl.out}/lib/libssl.so'"
   '';
 
   meta = with lib; {
