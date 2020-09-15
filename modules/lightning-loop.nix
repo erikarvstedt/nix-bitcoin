56705fbd16c98e2b9f3f1098a6c994f19a97f47b
nixbitcoin <nixbitcoin@i2pmail.org>
2020-09-15 08:46:15 +0000

    lightning-loop: update module
    * commandlineArgs -> configFile
    * introduce tls certs
    * loop dataDir
    
    Warning: Manual migration of existing loop data directory necessary

--- modules/lightning-loop.nix
+++ modules/lightning-loop.nix
@@ -6,6 +6,20 @@ let
   cfg = config.services.lightning-loop;
   inherit (config) nix-bitcoin-services;
   secretsDir = config.nix-bitcoin.secretsDir;
+  configFile = pkgs.writeText "loopd.conf" ''
+    datadir=${cfg.dataDir}
+    logdir=${cfg.dataDir}/logs
+    tlscertpath=${secretsDir}/loop-cert
+    tlskeypath=${secretsDir}/loop-key
+
+    lnd.host=${config.services.lnd.listen}:10009
+    lnd.macaroondir=${config.services.lnd.dataDir}/chain/bitcoin/mainnet
+    lnd.tlspath=${secretsDir}/lnd-cert
+
+    ${optionalString (cfg.proxy != null) "server.proxy=${cfg.proxy}"}
+
+    ${cfg.extraConfig}
+  '';
 in {
 
   options.services.lightning-loop = {
@@ -16,21 +30,29 @@ in {
       defaultText = "pkgs.nix-bitcoin.lightning-loop";
       description = "The package providing lightning-loop binaries.";
     };
+    dataDir = mkOption {
+      type = types.path;
+      default = "/var/lib/lightning-loop";
+      description = "The data directory for Loop.";
+    };
     proxy = mkOption {
       type = types.nullOr types.str;
       default = null;
       description = "Connect through SOCKS5 proxy";
     };
-    extraArgs = mkOption {
-      type = types.separatedString " ";
+    extraConfig = mkOption {
+      type = types.lines;
       default = "";
+      example = ''
+        debuglevel=trace
+      '';
       description = "Extra command line arguments passed to loopd.";
     };
     cli = mkOption {
       default = pkgs.writeScriptBin "loop"
       # Switch user because lnd makes datadir contents readable by user only
       ''
-        ${cfg.cliExec} sudo -u lnd ${cfg.package}/bin/loop "$@"
+        ${cfg.cliExec} sudo -u lnd ${cfg.package}/bin/loop --tlscertpath ${secretsDir}/loop-cert "$@"
       '';
       description = "Binary to connect with the lnd instance.";
     };
@@ -47,27 +69,33 @@ in {
 
     environment.systemPackages = [ cfg.package (hiPrio cfg.cli) ];
 
+    systemd.tmpfiles.rules = [
+      "d '${cfg.dataDir}' 0770 lnd lnd - -"
+    ];
+
     systemd.services.lightning-loop = {
       description = "Run loopd";
       wantedBy = [ "multi-user.target" ];
       requires = [ "lnd.service" ];
       after = [ "lnd.service" ];
+      preStart = ''
+        install -m600 ${configFile} '${cfg.dataDir}/loopd.conf'
+      '';
       serviceConfig = nix-bitcoin-services.defaultHardening // {
-        ExecStart = ''
-          ${cfg.package}/bin/loopd \
-          --lnd.host=${config.services.lnd.listen}:10009 \
-          --lnd.macaroondir=${config.services.lnd.dataDir}/chain/bitcoin/mainnet \
-          --lnd.tlspath=${secretsDir}/lnd-cert \
-          ${optionalString (cfg.proxy != null) "--server.proxy=${cfg.proxy}"} \
-          ${cfg.extraArgs}
-        '';
+        WorkingDirectory = cfg.dataDir;
+        ExecStart = "${cfg.package}/bin/loopd --configfile=${cfg.dataDir}/loopd.conf";
         User = "lnd";
         Restart = "on-failure";
         RestartSec = "10s";
-        ReadWritePaths = "${config.services.lnd.dataDir}";
+        ReadWritePaths = "${cfg.dataDir}";
       } // (if cfg.enforceTor
           then nix-bitcoin-services.allowTor
           else nix-bitcoin-services.allowAnyIP);
     };
+
+     nix-bitcoin.secrets = {
+       loop-key.user = "lnd";
+       loop-cert.user = "lnd";
+     };
   };
 }
