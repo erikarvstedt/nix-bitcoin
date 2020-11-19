af328b1b8330f6aa9a90266db4a2fa954829a887
Erik Arvstedt <erik.arvstedt@gmail.com>
2020-11-19 01:16:05 +0100

    fixup! move trivial plugins to default.nix

--- modules/clightning-plugins/default.nix
+++ modules/clightning-plugins/default.nix
@@ -1,12 +1,26 @@
 { config, lib, pkgs, ... }:
 
-{
+let
+  cfg = config.services.clightning.plugins;
+  pluginPkgs = config.nix-bitcoin.pkgs.clightning-plugins;
+in {
   imports = [
-    ./helpme.nix
-    ./monitor.nix
     ./prometheus.nix
-    ./rebalance.nix
     ./summary.nix
     ./zmq.nix
   ];
+
+  options.services.clightning.plugins = {
+    helpme.enable = lib.mkEnableOption "Help me (clightning plugin)";
+    monitor.enable = lib.mkEnableOption "Monitor (clightning plugin)";
+    rebalance.enable = lib.mkEnableOption "Rebalance (clightning plugin)";
+  };
+
+  config = {
+    services.clightning.extraConfig = lib.mkMerge [
+      (lib.mkIf cfg.helpme.enable "plugin-dir=${pluginPkgs.helpme}")
+      (lib.mkIf cfg.monitor.enable "plugin-dir=${pluginPkgs.monitor}")
+      (lib.mkIf cfg.rebalance.enable "plugin-dir=${pluginPkgs.rebalance}")
+    ];
+  };
 }
