0c949518b409be3e8555c0fed4570ea546cfe265
Erik Arvstedt <erik.arvstedt@gmail.com>
2020-12-10 19:21:34 +0100

    use cache nix-bitcoin-ci-ea

--- ci/build.sh
+++ ci/build.sh
@@ -11,7 +11,7 @@ set -euo pipefail
 
 scenario=${scenario:-}
 CACHIX_SIGNING_KEY=${CACHIX_SIGNING_KEY:-}
-cachixCache=nix-bitcoin
+cachixCache=nix-bitcoin-ci-ea
 
 trap 'echo Error at line $LINENO' ERR
 
