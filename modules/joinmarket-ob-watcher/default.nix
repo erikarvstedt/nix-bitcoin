360cfde02ae7b442d1f86bebf5bb59af7cb2f4f0
Erik Arvstedt <erik.arvstedt@gmail.com>
2021-01-01 14:41:28 +0100

    fixup! remove dependency on tor control socket
    as i understand it, this is only needed for payjoin.

--- modules/joinmarket-ob-watcher/default.nix
+++ modules/joinmarket-ob-watcher/default.nix
@@ -62,7 +62,6 @@ in {
     services.tor = {
       enable = true;
       client.enable = true;
-      controlSocket.enable = true;
     };
 
     systemd.services.joinmarket-ob-watcher = {
@@ -89,7 +88,6 @@ in {
     users.users.${cfg.user} = {
       group = cfg.group;
       home = cfg.dataDir; # The service writes to HOME/.config/matplotlib
-      extraGroups = [ "tor" ];
     };
     users.groups.${cfg.group} = {};
   };
