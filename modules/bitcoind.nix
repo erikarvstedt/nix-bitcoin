9b6e0704d005835b6f79e3789637fa5b70ef5e3f
Erik Arvstedt <erik.arvstedt@gmail.com>
2021-02-01 11:42:40 +0100

    treewide: remove user descriptions
    User descriptions are stored in the `comment` field in /etc/passwd.
    In our case, these are completely redundant and don't add any useful information.

--- modules/bitcoind.nix
+++ modules/bitcoind.nix
@@ -381,10 +381,7 @@ in {
       } // nix-bitcoin-services.allowTor;
     };
 
-    users.users.${cfg.user} = {
-      group = cfg.group;
-      description = "Bitcoin daemon user";
-    };
+    users.users.${cfg.user}.group = cfg.group;
     users.groups.${cfg.group} = {};
     users.groups.bitcoinrpc = {};
     nix-bitcoin.operator.groups = [ cfg.group ];
