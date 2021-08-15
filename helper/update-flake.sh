bb4363abde93df22c8b65f8ed5eacc8ef4898f03
Erik Arvstedt <erik.arvstedt@gmail.com>
2021-08-15 11:31:51 +0200

    add helper/update-flake.sh
    This greatly simplifies updating nixpkgs.
    See the comment at the top of update-flake.sh for a description.

---
+++ helper/update-flake.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# - Update all flake inputs, including nixpkgs
+# - Print version updates of pinned pkgs like so:
+#     Pkg updates in nixpkgs unstable:
+#     bitcoin: 0.20.0 -> 0.21.1
+#     btcpayserver: 1.1.0 -> 1.1.2
+# - Write ../pkgs/pinned.nix:
+#     Packages for which the stable und unstable versions are identical are
+#     pinned to stable.
+#     All other pkgs are pinned to unstable.
+
+# cd to script dir
+cd "${BASH_SOURCE[0]%/*}"
+
+if [[ ${1:-} != -f ]] && ! git diff --quiet ../flake.{nix,lock}; then
+    echo "error: flake.nix/flake.lock have changes. Run with option -f to ignore."
+    exit 1
+fi
+
+nix() {
+    env nix --experimental-features "nix-command flakes" "$@"
+}
+
+versions=$(nix eval --json -f update-flake.nix versions)
+
+## Uncomment the following to generate a version change message for testing
+# versions=$(echo "$versions" | sed 's|1|0|g')
+
+nix flake update ..
+
+echo
+nix eval --raw -f update-flake.nix --argstr prevVersions "$versions" showUpdates; echo
+
+pinned=../pkgs/pinned.nix
+pinnedSrc=$(nix eval --raw -f update-flake.nix --argstr prevVersions "$versions" pinnedFile)
+if ! cmp --quiet <(echo "$pinnedSrc") "$pinned"; then
+    echo "$pinnedSrc" > "$pinned"
+    echo
+    echo "Updated pinned.nix"
+fi
