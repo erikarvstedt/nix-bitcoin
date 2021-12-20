3a711a829f170c4b713964c8a4aa6795dc478dcb
Erik Arvstedt <erik.arvstedt@gmail.com>
2021-12-20 15:16:39 +0100

    helper: add start-bash-session.sh

---
+++ helper/start-bash-session.sh
@@ -0,0 +1,8 @@
+# Start an interactive bash session in the current bash environment.
+
+# BASH_ENVIRONMENT contains definitions of read-only variables like 'BASHOPTS' that
+# cause warnings on evaluation. Suppress these warnings while sourcing.
+#
+# shellcheck disable=SC2016
+BASH_ENVIRONMENT=<(declare -p; declare -pf) \
+  bash --rcfile <(echo 'source $BASH_ENVIRONMENT 2>/dev/null')
