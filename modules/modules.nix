08b0719843e6e4a5f6c51f9ae4181613d7047efc
Erik Arvstedt <erik.arvstedt@gmail.com>
2022-07-29 15:37:45 +0200

    fixup! rename clightning-db-replication.nix -> clightning-replication.nix
    Now the file name matches the options path (`clighting.replication`)

--- modules/modules.nix
+++ modules/modules.nix
@@ -13,7 +13,7 @@
     ./clightning.nix
     ./clightning-plugins
     ./clightning-rest.nix
-    ./clightning-db-replication.nix
+    ./clightning-replication.nix
     ./spark-wallet.nix
     ./lnd.nix
     ./lightning-loop.nix
