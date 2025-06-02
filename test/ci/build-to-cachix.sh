06a37dcdf10f4ccb164680c12ba2c9075cd2227a
Erik Arvstedt <erik.arvstedt@gmail.com>
2025-06-02 14:02:15 +0200

    [tmp] use different cachixCache to work around nix `NIX_SSL_CERT_FILE` bug
    I have access to this cache so I can push fixed-output drvs that fail
    to build in the CI environment.

--- test/ci/build-to-cachix.sh
+++ test/ci/build-to-cachix.sh
@@ -7,7 +7,7 @@
 set -euo pipefail
 
 CACHIX_SIGNING_KEY="${CACHIX_SIGNING_KEY:-}"
-cachixCache=nix-bitcoin
+cachixCache=nix-bitcoin-ci-ea
 
 trap 'echo "Error at ${BASH_SOURCE[0]}:$LINENO"' ERR
 
--- test/nixos-search/ci-test.sh
+++ test/nixos-search/ci-test.sh
@@ -6,7 +6,7 @@ cd "${BASH_SOURCE[0]%/*}"
 nbFlake=$(realpath ../..)
 
 # Use cachix to cache the `flake-info` build
-cachixCache=nix-bitcoin
+cachixCache=nix-bitcoin-ci-ea
 
 nix run .#cachix -- use "$cachixCache"
 
